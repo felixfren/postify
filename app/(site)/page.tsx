d9eb54dec74951f06bc7ff9fe8c5e53b9b38d89a
felixfren <felix.535210052@stu.untar.ac.id>
2023-06-17 13:27:43 +0700

    home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">
-      Main Content
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome Back, Varye
+          </h1>
+          <div className="grid grid-cols sm:grid-cols-2 xl:grid-cols-4 gap-3 mt-4"> 
+
+          </div>
+        </div>
+      </Header>
     </div>
   )
 }
