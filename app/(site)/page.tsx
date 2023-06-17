38eb122ce1db086e3aff5466efe2dd73b23fa09d
felixfren <felix.535210052@stu.untar.ac.id>
2023-06-17 13:51:19 +0700

    liked songs

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -9,7 +10,11 @@ export default function Home() {
             Welcome Back, Varye
           </h1>
           <div className="grid grid-cols sm:grid-cols-2 xl:grid-cols-4 gap-3 mt-4"> 
-
+            <ListItem 
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
