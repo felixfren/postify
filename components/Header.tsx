5aececfd8147f81eb467db4ddb5942c2e303c5a6
felixfren <felix.535210052@stu.untar.ac.id>
2023-06-22 11:18:11 +0700

    modal + auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,8 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModel";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -16,7 +18,12 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter();
+
+    const supabaseClient = useSupabaseClient();
+
+    
 
     const handleLogout = () => {
         //handle logout
@@ -50,13 +57,15 @@ const Header: React.FC<HeaderProps> = ({
                 <div className="flex justify-between items-center gap-x-4">
                     <>
                         <div>
-                            <Button className="bg-transparent text-neutral-300 font-medium">
+                            <Button 
+                            onClick={authModal.onOpen}
+                            className="bg-transparent text-neutral-300 font-medium">
                                 Sign Up
                             </Button>
                         </div>
                         <div>
                             <Button 
-                            onClick={() => {}}
+                            onClick={authModal.onOpen}
                             className="bg-white px-6 py-2">
                                 Log in
                             </Button>
