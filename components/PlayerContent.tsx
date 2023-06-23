eda2701dd93cef75b8e3dad52519e65eef86462c
felixfren <felix.535210052@stu.untar.ac.id>
2023-06-23 18:09:13 +0700

    playback

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -8,6 +8,9 @@ import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
 
 interface PlayerContentProps {
     song: Song;
@@ -18,8 +21,81 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     song,
     songUrl
 }) => {
-    const Icon = true ? BsPauseFill : BsPlayFill;
-    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+    const player = usePlayer();
+    const [volume, setVolume] = useState(1);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+    const onPlayNext = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const nextSong = player.ids[currentIndex + 1];
+
+        if (!nextSong) {
+            return player.setId(player.ids[0]);
+        }
+
+        player.setId(nextSong);
+    }
+
+    const onPlayPrevious = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const previousSong = player.ids[currentIndex - 1];
+
+        if (!previousSong) {
+            return player.setId(player.ids[player.ids.length - 1]);
+        }
+
+        player.setId(previousSong);  
+    };
+
+    const [play, { pause, sound }] = useSound(
+        songUrl,
+        {
+            volume: volume,
+            onplay: () => setIsPlaying(true),
+            onend: () => {
+                setIsPlaying(false);
+                onPlayNext();
+            },
+            onpause: () => setIsPlaying(false),
+            format: ['mp3']
+        }
+    );
+
+    useEffect(() => {
+        sound?.play();
+
+        return () => {
+            sound?.unload();
+        }
+    }, [sound]);
+
+    const handlePlay = () => {
+        if (!isPlaying) {
+            play();
+        } else {
+            pause();
+        }
+    };
+
+    const toggleMute = () => {
+        if (volume === 0) {
+            setVolume(1);
+        } else {
+            setVolume(0);
+        }
+    }
+
     return ( 
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
             <div className="flex w-full justify-start">
@@ -39,7 +115,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
 
                 <AiFillStepBackward 
-                onClick={() => {}}
+                onClick={onPlayPrevious}
                 size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
 
                 <div 
@@ -49,18 +125,21 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 </div>
 
                 <AiFillStepForward 
-                onClick={() => {}}
+                onClick={onPlayNext}
                 size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
 
             <div className="hidden md:flex w-full justify-end pr-2">
                 <div className="flex items-center gap-x-2 w-[120px]">
                     <VolumeIcon 
-                        onClick={() => {}}
+                        onClick={toggleMute}
                         className="cursor-pointer"
                         size={34}
                     />
-                    <Slider />
+                    <Slider 
+                        value={volume}
+                        onChange={(value) => setVolume(value)}
+                    />
                 </div>
             </div>
         </div>
