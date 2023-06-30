cf06c0e0c596713141613759b94434b15e0770ad
felixfren <felix.535210052@stu.untar.ac.id>
2023-06-30 12:09:36 +0700

    seek slider done

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -12,6 +12,7 @@ import usePlayer from "@/hooks/usePlayer";
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
 import PlayingMediaItem from "./PlayingMediaItem";
+import SeekSlider from "./SeekSlider";
 
 interface PlayerContentProps {
     song: Song;
@@ -59,7 +60,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         player.setId(previousSong);  
     };
 
-    const [play, { pause, sound }] = useSound(
+    const [play, { pause, duration, sound }] = useSound(
         songUrl,
         {
             volume: volume,
@@ -73,9 +74,51 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         }
     );
 
+    const [time, setTime] = useState({
+        min: "00", sec: "00"
+    });
+
+    const [currTime, setCurrTime] = useState({
+        min: "00", sec: "00"
+    });
+
+    const [seconds, setSeconds] = useState(0);
+
+    //useEffect for duration calculations
+    useEffect(() => {
+        if (duration) {
+            const sec = duration / 1000;
+            const min = Math.floor(sec / 60);
+            const secRemain = Math.floor(sec % 60);
+            setTime({
+                min: isNaN(min) ? "00" : min.toString().padStart(2, '0'),
+                sec: isNaN(sec) ? "0" : secRemain.toString().padStart(2, '0')
+            });
+        }
+    },[isPlaying, duration, setTime]);
+
+    //useEffect for seek timer
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (sound){
+                setSeconds(sound.seek([]));
+                const min = Math.floor(sound.seek([]) / 60);
+                const sec = Math.floor(sound.seek([]) % 60);
+                setCurrTime({
+                    min: isNaN(min) ? "00" : min.toString().padStart(2, '0'),
+                    sec: isNaN(sec) ? "0" : sec.toString().padStart(2, '0')
+                });
+            }
+        }, 1000);
+        return () => {
+            sound?.unload();
+            clearInterval(interval);
+        };
+    },[sound, setSeconds, setCurrTime])
+
+    //useEffect for audio playback
     useEffect(() => {
         sound?.play();
-
         return () => {
             sound?.unload();
         }
@@ -113,21 +156,41 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 
             </div>
 
-            <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
+            <div className="hidden h-full grid-cols-2 flex-col md:flex justify-center items-center w-full max-w-[722px] gap-y-2">
+                <div className="flex items-center gap-x-6">
+                    <AiFillStepBackward 
+                    onClick={onPlayPrevious}
+                    size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
 
-                <AiFillStepBackward 
-                onClick={onPlayPrevious}
-                size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+                    <div 
+                        onClick={handlePlay}
+                        className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+                        <Icon size={30} className="text-black" /> 
+                    </div>
 
-                <div 
-                    onClick={handlePlay}
-                    className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
-                    <Icon size={30} className="text-black" /> 
+                    <AiFillStepForward 
+                    onClick={onPlayNext}
+                    size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
                 </div>
-
-                <AiFillStepForward 
-                onClick={onPlayNext}
-                size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+                <div className="flex items-center gap-x-6">
+                    <p>
+                        {currTime.min}:{currTime.sec}
+                    </p>
+                    <SeekSlider 
+                        max={duration ? duration / 1000 : 0}
+                        value={seconds}
+                        onChange={(value) => {
+                            pause();
+                            setSeconds(value);
+                            sound.seek([value]);
+                        }}
+                        onCommit={play}
+                    />
+                    <p>
+                        {time.min}:{time.sec}
+                    </p>
+                </div>
+                
             </div>
 
             <div className="hidden md:flex w-full justify-end pr-2">
